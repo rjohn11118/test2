beaf9a1b2ee0bafc83ff674c5a9449ad7b58bf07
River <river.johnson@stu.barren.kyschools.us>
2024-09-26 09:48:46 -0500

    Initial Commit

---
+++ test2/test2App.swift
@@ -0,0 +1,17 @@
+//
+//  test2App.swift
+//  test2
+//
+//  Created by Student on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
